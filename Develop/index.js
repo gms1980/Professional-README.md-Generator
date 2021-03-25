14771d2a65bd6ad7f270ba7d13ae11baf4be4a1e
George <gms1980@icloud.com>
2021-03-25 11:45:04 -0700

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,31 +1,102 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const generatePage = require('potential-enigma/Develop/utils/generateMarkdown.js');
+console.log(generatePage());
 // TODO: Create an array of questions for user input
 const questions = () => {
-    return inquirer.prompt([
-    {
+    return inquirer
+    .prompt([
+      {
         type: 'input',
-        Name: "Name",
-        message: "What is the name of your project?",
+        name: 'name',
+        message: 'What is the name of your project? (Required)',
         validate: nameInput => {
-           if (nameInput) {
-               return true;
-            } else {
-            console.log('Please enter the Title of your project');
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project name!');
             return false;
-           }
+          }
         }
-    },
-    
-    
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project description!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What steps are required to install your project (Required)?',
+        validate: installationInput => {
+          if (installationInput) {
+            return true;
+          } else {
+            console.log('You need to enter a steps!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use (Required)',
+        validate: usageInput => {
+          if (usageInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project description!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: linkInput => {
+          if (linkInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project GitHub link!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+      }
     ]);
 };
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
