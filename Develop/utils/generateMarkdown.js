b5ff7c2cdf2e70cd7c75ccb8ce16abbf8c3d407b
George <gms1980@icloud.com>
2021-03-27 22:06:08 -0700

    added readme markdown template

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,24 +1,57 @@
-//const fs = require('fs');
+const fs = require('fs');
 
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  ## Licensing:
+  
+
+  
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [Badges](#badges)
+  - [Additional Info](#additional-info)
+  ## Description:
+  ${data.description}
+  ## Installation:
+  ${data.installation}
+  ## Usage:
+  ${data.usage}
+  ## Credits:
+  ${data.credits}
+  ## Contribution:
+  ${data.contribution}
+  ## Badges:
+  ${data.badges}
+  ## Testing:
+  ${data.testing}
+  ## Name:
+  ${data.name}
+  ## Github: [${data.github}](https://github.com/${data.github})
+  ## Email: ${data.email} `;
+}
+
+module.exports = generateMarkdown;
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+//function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(userResponses, userInfo) {
-  //return `# ${data.title} figure out return after var
+//function generateMarkdown(userResponses, userInfo) {
+  // return # ${data.title} figure out return after var
 //Table of Contents Here    
 //   let toc = `tableOfContents`
 
 //     if 
-}
 
-//module.exports = generateMarkdown;
+
+
