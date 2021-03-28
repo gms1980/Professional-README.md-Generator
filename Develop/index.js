1b24d6997db3aed3786ea28219d82765e1b0f8b3
George <gms1980@icloud.com>
2021-03-27 19:31:33 -0700

    cleaned up questions array

--- Develop/index.js
+++ Develop/index.js
@@ -12,68 +12,29 @@ const questions = () => {
     .prompt([
       {
         type: 'input',
-        name: 'name',
-        message: 'What is the name of your project? (Required)',
-        validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log('You need to enter a project name!');
-            return false;
-          }
-        }
+        name: 'title',
+        message: 'What is the name of your project? (Required)',       
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)',
-        validate: descriptionInput => {
-          if (descriptionInput) {
-            return true;
-          } else {
-            console.log('You need to enter a project description!');
-            return false;
-          }
-        }
+        message: 'Provide a description of the project (Required)',        
       },
       {
         type: 'input',
         name: 'installation',
-        message: 'What steps are required to install your project (Required)?',
-        validate: installationInput => {
-          if (installationInput) {
-            return true;
-          } else {
-            console.log('You need to enter a steps!');
-            return false;
-          }
-        }
+        message: 'What steps are required to install your project (Required)?',       
       },
       {
         type: 'input',
         name: 'usage',
         message: 'Provide instructions and examples for use (Required)',
-        validate: usageInput => {
-          if (usageInput) {
-            return true;
-          } else {
-            console.log('You need to enter a project description!');
-            return false;
-          }
-        }
       },
       {
         type: 'input',
         name: 'credits',
         message: 'List your Collaborators, if any, and links to their GitHub profiles',
-        validate: creditsInput => {
-          if (creditsInput) {
-            return true;
-          } else {
-            console.log('List those that you collaborated with');
-            return false;
-          }
-        }
+        
       },
       {
         type: 'confirm',
@@ -84,28 +45,25 @@ const questions = () => {
       {
         type: 'input',
         name: 'features',
-        message: 'Did you want to present any features?',
-        validate: featuresInput => {
-          if (featuresInput) {
-            return true;
-          } else {
-            console.log('You need to enter a steps!');
-            return false;
-          }
-        }
+        message: 'Did you want to present any features?',        
       },
       {
-        type: 'confirm',
-        name: 'feature',
-        message: 'Would you like to feature this project?',
-        default: false
+        type: 'checkbox',
+        name: 'licenses',
+        message: 'What licenses did you use with this project',
+        choices: ['MIT', 'Other']
+        
       },
       {
-        type: 'confirm',
-        name: 'confirmAddProject',
-        message: 'Would you like to enter another project?',
-        default: false
-      }
+        type: 'input',
+        name: 'name',
+        message: 'Enter your name',          
+      }, 
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email',
+      },    
     ]);
 };
 
@@ -119,9 +77,9 @@ function init() {
 }
 init();
 
-// const READMEfile = '# ${name}'
 
-// fs.writeFile('./utils/README.md', README.md), err => {
+
+// fs.writeFile('./utils/README.md', questions), err => {
 //     if (err) throw err;
 //     console.log ('saved!');
     
